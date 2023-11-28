fb85fbcc085f45f96408da75831cbd75d36006ca
= <=>
2023-11-28 11:53:41 +0100

    diferents rutes afegides + arxius(sense contingut)

---
+++ frontEnd/src/views/LandingView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+    <div>
+        <RouterLink to="/sales">
+            
+        </RouterLink>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
