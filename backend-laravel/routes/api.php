4009c3064686eeee3ae6d37f520ec8b96cad2794
santipu03 <santipujolsau@gmail.com>
2023-11-28 13:03:36 +0100

    Afegit Sanctum is creades rutes per a usuaris

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PreguntesController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,15 @@ use App\Http\Controllers\PreguntesController;
 |
 */
 
+// Public Routes
 Route::post('/getPregunta', [PreguntesController::class, 'search']);
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class,'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
