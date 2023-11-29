578af57d6dc719619c14c343be4c78ee0c9c5859
santipu03 <santipujolsau@gmail.com>
2023-11-29 12:08:17 +0100

    Crida API a laravel i refactor

---
+++ backend-node/communicationManager.js
@@ -0,0 +1,15 @@
+export default async function getPregunta(dificultat, categoria, preguntesAnteriors) {
+    const response = await fetch('http://localhost:8000/api/getPregunta', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            dificultat: dificultat,
+            categoria: categoria,
+            preguntesAnteriors: preguntesAnteriors
+        }),
+    });
+    const jsonResponse = await response.json()
+    return jsonResponse
+}
