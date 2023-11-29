c157cf435779bdae16600af33d4352ffafb6593f
= <=>
2023-11-29 13:20:41 +0100

    grafic acabat

---
+++ frontEnd/src/views/ResultatsView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Resultats</h1>
+        <BarChart :chart-data="{
+            labels: ['Donald Trump', 'Joe Biden', 'Robert Kennedy Jr.', 'Michelle Obama'],
+            datasets: [{
+                data: votos,
+                label: 'Votos',
+                backgroundColor: '#f87979',
+            }]
+        }" />
+    </div>
+</template>
+
+<script>
+
+import BarChart from './BarChart.vue';
+
+export default {
+
+    components: { BarChart },
+    
+    
+
+}
+</script>
+
+<style lang="scss" scoped></style>
