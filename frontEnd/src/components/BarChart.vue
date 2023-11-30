98e9004554edfa02977eae7ea929c2009857f1e1
= <=>
2023-11-30 08:39:12 +0100

    grafic acabat

--- frontEnd/src/components/BarChart.vue
+++ frontEnd/src/components/BarChart.vue
@@ -1,16 +1,16 @@
 <template>
-    <Bar id="my-chart-id" :options="chartOptions" :data="chartData"/>
+    <Pie id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
   
 <script>
-import { Bar } from 'vue-chartjs'
+import { Pie } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
     name: 'BarChart',
-    components: { Bar },
+    components: { Pie },
     props: {
         chartData: {
             type: Object,
@@ -20,9 +20,15 @@ export default {
     data() {
         return {
             chartOptions: {
-                responsive: true
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top'
+                    }
+                }
             }
+
         }
-    },
+    }
 }
-</script>
+</script>                                                                                                                                                                                                                                                                   
--- frontEnd/src/views/ResultatsView.vue
+++ frontEnd/src/views/ResultatsView.vue
@@ -1,26 +1,40 @@
 <template>
     <div>
         <h1>Resultats</h1>
-        <BarChart :chart-data="{
-            labels: ['Resposta 1', 'Resposta 2', 'Resposta 3', 'Resposta 4'],
-            datasets: [{
-                data: votos,
-                label: 'Respostes',
-                backgroundColor: '#f87979',
-            }]
-        }" />
+        <div>
+            <h2>Equip 1</h2>
+            <BarChart :chart-data="{
+                labels: ['Resposta 1', 'Resposta 2', 'Resposta 3', 'Resposta 4'],
+                datasets: [{
+                    data: resE1,
+                    label: 'Respostes',
+                    backgroundColor: '#f87979',
+                }]
+            }" />
+        </div>
+        <div>
+            <h2>Equip 2</h2>
+            <BarChart :chart-data="{
+                labels: ['Resposta 1', 'Resposta 2', 'Resposta 3', 'Resposta 4'],
+                datasets: [{
+                    data: resE2,
+                    label: 'Respostes',
+                    backgroundColor: '#f87979',
+                }]
+            }" />
+        </div>
     </div>
 </template>
 
 <script>
+
+import { useAppStore } from '../stores/app';
 
 import BarChart from './BarChart.vue';
 
 export default {
 
     components: { BarChart },
-    
-    
 
 }
 </script>
