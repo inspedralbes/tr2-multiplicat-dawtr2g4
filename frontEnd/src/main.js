de8e2c2994d718a5953af1e295a1791427fd09dc
santipu03 <santipujolsau@gmail.com>
2023-12-05 10:03:42 +0100

    Millorat estils header, footer i landing

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -2,8 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'primevue/resources/themes/saga-blue/theme.css'; // theme
-import 'primevue/resources/primevue.min.css'; // core css
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
 import "primeflex/primeflex.css";
 
 import App from './App.vue'
--- frontEnd/src/views/PartidaView.vue
+++ frontEnd/src/views/PartidaView.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
-        <h1>Ara mateix batejant EQUIP {{ salaInfo.equipAtacant }}</h1>
-    </div>
-    <div id="flex-container">
-        <div id="container">
-            <img class="jugador home-base" id="jugador-0" src="/img/jugador.png" alt="jugador">
-            <img class="camp" src="/img/camp.jpg" alt="">
+        <div>
+            <h1>Ara mateix batejant EQUIP {{ salaInfo.equipAtacant }}</h1>
         </div>
-        <div id="puntuacio">
-            <p>EQUIP 1: {{ salaInfo.equips[0].punts }}</p>
-            <p>EQUIP 2: {{ salaInfo.equips[1].punts }}</p>
-        </div>
-        <div id="moviment-bases">
-            <button @click="initVotacio">COMENÇAR VOTACIÓ</button>
-            <p>{{ temporitzador }}</p>
-            <div v-if="votacioBaseEnCurs == true && equip == salaInfo.equipAtacant">
-                <p>Quantes bases us voleu moure?</p>
-                <div v-for="index in 3" :key="index">
-                    <button v-on:click="baseSeleccionada(index)">{{ index }}</button>
+        <div id="flex-container">
+            <div id="container">
+                <img class="jugador home-base" id="jugador-0" src="/img/jugador.png" alt="jugador">
+                <img class="camp" src="/img/camp.jpg" alt="">
+            </div>
+            <div id="puntuacio">
+                <p>EQUIP 1: {{ salaInfo.equips[0].punts }}</p>
+                <p>EQUIP 2: {{ salaInfo.equips[1].punts }}</p>
+            </div>
+            <div id="moviment-bases">
+                <button @click="initVotacio">COMENÇAR VOTACIÓ</button>
+                <p>{{ temporitzador }}</p>
+                <div v-if="votacioBaseEnCurs == true && equip == salaInfo.equipAtacant">
+                    <p>Quantes bases us voleu moure?</p>
+                    <div v-for="index in 3" :key="index">
+                        <button v-on:click="baseSeleccionada(index)">{{ index }}</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -27,7 +29,7 @@
 <script>
 //import io from 'socket.io-client';
 
-import {useAppStore} from '../stores/app'
+import { useAppStore } from '../stores/app'
 import { socket } from '@/socket';
 import { watch } from 'vue'
 export default {
@@ -40,7 +42,7 @@ export default {
     methods: {
         baseSeleccionada(idBase) {
             if (idBase == 1 | idBase == 2 | idBase == 3) {
-                console.log("Has pulsado " + idBase);       
+                console.log("Has pulsado " + idBase);
                 socket.emit('vot-dificultat', this.indexSala, idBase);
             }
         },
@@ -68,7 +70,7 @@ export default {
                 jugador.classList.add("home-base");
             }
         },
-        initVotacio(){  
+        initVotacio() {
             socket.emit('començar-votacio-dificultat', this.indexSala);
         }
     },
@@ -98,7 +100,7 @@ export default {
 
         this.pinia.$subscribe((mutation, state) => {
 
-            if(this.pinia.jugadorEnCamp.baseActual != 0) {
+            if (this.pinia.jugadorEnCamp.baseActual != 0) {
                 this.pintarCamp();
             }
         });
@@ -108,9 +110,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-:root{
+:root {
     --timeMov: 0.75s;
 }
+
 #flex-container {
     display: flex;
     justify-content: center;
@@ -150,7 +153,8 @@ export default {
 }
 
 .moviment {
-  transition: all var(--timeMov) ease; /* Puedes ajustar la duración y la función de temporización según tus necesidades */
+    transition: all var(--timeMov) ease;
+    /* Puedes ajustar la duración y la función de temporización según tus necesidades */
 }
 
 .home-base {
