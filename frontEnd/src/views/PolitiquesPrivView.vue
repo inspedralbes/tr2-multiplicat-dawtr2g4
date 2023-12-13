df7a60edb972a1a41bb69ccd29c16e9f3af03f70
= <=>
2023-12-13 08:32:52 +0100

    acabat de fer el commit de la vista de politiques de privacitat

---
+++ frontEnd/src/views/PolitiquesPrivView.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <header>
+            <h1>Política de Privacitat</h1>
+        </header>
+
+        <main>
+            <section>
+                <h2>Informació recopilada</h2>
+                <p>Al nostre lloc web, recopilem informació personal només quan la proporciones voluntàriament. Aquesta
+                    informació pot incloure el teu nom, adreça de correu electrònic, adreça postal, etc.</p>
+            </section>
+
+            <section>
+                <h2>Ús de la informació</h2>
+                <p>La informació recopilada s'utilitza per processar comandes, personalitzar la teva experiència al lloc,
+                    enviar correus electrònics promocionals i millorar els nostres serveis.</p>
+            </section>
+
+            <section>
+                <h2>Protecció de la informació</h2>
+                <p>La teva informació personal està protegida i no serà venuda, intercanviada, transferida o proporcionada a
+                    cap altra empresa sense el teu consentiment, excepte per al propòsit de complir amb una sol·licitud i/o
+                    transacció.</p>
+            </section>
+
+            <section>
+                <h2>Cookies</h2>
+                <p>El nostre lloc web utilitza cookies per millorar la teva experiència. Pots configurar el teu navegador
+                    per rebutjar totes les cookies o per avisar-te quan s'envii una cookie.</p>
+            </section>
+
+            <section>
+                <h2>Imatges generades per IA</h2>
+                <p>Totes les imatges del nostre lloc web estan generades per intel·ligència artificial (IA).</p>
+            </section>
+
+            <section>
+                <h2>Enllaços a tercers</h2>
+                <p>Ocasionalment, al nostre criteri, podem incloure o oferir productes o serveis de tercers al nostre lloc
+                    web. Aquests llocs de tercers tenen polítiques de privacitat separades i independents. Per tant, no
+                    tenim responsabilitat ni obligació pel contingut i les activitats d'aquests llocs enllaçats.</p>
+            </section>
+
+            <section>
+                <h2>Canvis a la nostra política de privacitat</h2>
+                <p>Si decidim canviar la nostra política de privacitat, actualitzarem la data de modificació a continuació.
+                </p>
+                <p>Última modificació: 12/12/2023</p>
+            </section>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header {
+    padding: 20px;
+    background-color: #f5f5f5;
+    border-bottom: 1px solid #ddd;
+    position: sticky;
+    top: 0;
+}
+
+header h1 {
+    margin: 0;
+    color: #333;
+    font-size: 2em;
+}
+
+main {
+    padding: 20px;
+}
+
+section {
+    margin-bottom: 20px;
+}
+
+h2 {
+    color: #333;
+    font-size: 1.5em;
+}
+
+p {
+    color: #666;
+    line-height: 1.6;
+}
+</style>
