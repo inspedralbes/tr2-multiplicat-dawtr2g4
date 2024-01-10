835ba3460b4a370cb5c4ee44ee64c2b861c4ec3f
Alvaro Kumenius <akumenius@gmail.com>
2024-01-10 12:38:17 +0100

    Modificar estils camp de joc

--- frontEnd/src/views/PartidaView.vue
+++ frontEnd/src/views/PartidaView.vue
@@ -137,6 +137,10 @@
                         </button>
                     </div>
                 </div>
+                <div class="loader_container" v-if="votacioBaseEnCurs == true && equip == equipDefensor">
+                    <p class="mt-6">Esperant la decisió de l'equip contrari...</p>
+                    <span class="loader mt-4"></span>
+                </div>
             </div>
         </div>
     </div>
@@ -331,7 +335,7 @@ export default {
 
 .equip-batejador{
     margin: 0;
-    padding: 12px;
+    padding: 6px;
     text-align: center;
     color: white;
 }
@@ -395,7 +399,7 @@ export default {
     width: fit-content;
     height: 30px;
     background-color: gray;
-    margin: 20px 0px 10px 20px;
+    margin: 0px 0px 15px 20px;
 }
 
 .llum-item {
@@ -497,7 +501,7 @@ export default {
 
 .temporitzador {
     position: absolute;
-    top: 0;
+    top: -0.15em;
     left: 31%;
 }
 
@@ -641,7 +645,7 @@ export default {
 .home-base {
     position: absolute;
     bottom: 1%;
-    left: 46.75%;
+    left: 45.75%;
 }
 
 .primera-base {
@@ -653,13 +657,13 @@ export default {
 .segona-base {
     position: absolute;
     bottom: 83%;
-    left: 46.75%;
+    left: 45.75%;
 }
 
 .tercera-base {
     position: absolute;
     bottom: 45%;
-    left: 25%;
+    left: 23.5%;
 }
 
 /* ANIMACIONS DIFICULTAT VOTACIONS*/
@@ -771,4 +775,50 @@ export default {
         opacity: 0;
     }
 }
+
+.loader_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.loader {
+  width: 10em;
+  height: 10em;
+  border: 0.5em solid #D3D3D3;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  animation: rotation 3.5s linear infinite;
+}
+.loader::after,
+.loader::before {
+  content: '';  
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-image: url('/img/pilota-beisbol-cronometre.png');
+  background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  width: 1em;
+  height: 1em;
+  z-index: 2;
+}
+.loader::before {
+  left: auto;
+  top: auto;
+  right: 0;
+  bottom: 0;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+} 
 </style>
